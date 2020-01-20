2b11ffc01b7076d2da188c1c5371c1a9b4a4ca5f
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2020-01-20 20:03:13 +0100

    Update server
    Add error handling

--- server.js
+++ server.js
@@ -2,26 +2,42 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const { PORT = 8000 } = process.env;
+const { PORT = 8080 } = process.env;
 
 const STATIC_DIR = 'prod';
 
 const readFile = fs.readFileSync;
 
+const contentTypeHeaders = new Map([
+  ['html', 'text/html'],
+  ['css', 'text/css'],
+  ['js', 'application/javascript'],
+]);
+
+const getContentTypeHeader = contentType => [
+  'Content-Type',
+  contentTypeHeaders.get(contentType),
+];
+
 const server = http.createServer((req, res) => {
-  switch (req.url) {
-    case '/': {
-      res.setHeader('Content-Type', 'text/html');
-      res.end(readFile(path.resolve(__dirname, STATIC_DIR, 'index.html')));
-      break;
+  const { url } = req;
+  let file;
+
+  try {
+    if (url === '/') {
+      file = readFile(path.resolve(__dirname, STATIC_DIR, 'index.html'));
+      res.setHeader(...getContentTypeHeader('html'));
+    } else {
+      const ext = (url.match(/(?!=\.)\w+$/) || [])[0];
+      if (contentTypeHeaders.has(ext)) {
+        file = readFile(path.resolve(__dirname, STATIC_DIR, req.url.slice(1)));
+        res.setHeader(...getContentTypeHeader(ext));
+      }
     }
-    default: {
-      const file = readFile(
-        path.resolve(__dirname, STATIC_DIR, req.url.slice(1)),
-      );
-      res.end(file);
-      break;
-    }
+
+    res.end(file);
+  } catch (err) {
+    res.end(err.toString());
   }
 });
 
